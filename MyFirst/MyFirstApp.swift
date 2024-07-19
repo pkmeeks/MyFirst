75c6bc91a80aed22fc8ff51332a22e4ecf3fc23d
pkmeeks <kipp.meeks@gmail.com>
2024-07-19 08:43:35 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Kipp Meeks on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
